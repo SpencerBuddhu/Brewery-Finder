b971f9f643945d41efed07575657950f0e323498
Ricardo Cruz <rickblender@hotmail.com>
2022-12-08 16:15:06 -0600

    added more beers to test data

--- capstone/java/database/data.sql
+++ capstone/java/database/data.sql
@@ -2,10 +2,10 @@ BEGIN TRANSACTION;
 
 -- USERS
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
-INSERT INTO users (username,password_hash,role) VALUES ('ricardo', '$2a$10$afuUAXrV/kKQjkr7v1OLg.AsebZxt2lg4rzhoUMTxSsP/Jqfw4MUS', 'ROLE_BEERLOVER');
-INSERT INTO users (username,password_hash,role) VALUES ('jane', '$2a$10$aFWHuZWqFAyl8b.ou2fzFuMvXfRJ/HqI1uM85Ah0N6TxxVBmOG4O.', 'ROLE_BEERLOVER');
-INSERT INTO users (username,password_hash,role) VALUES ('spencer', '$2a$10$U0q1o1SW5nCa6BHlfNqLHug8J0mcOjv6zBcnV66OYbJamaID.HIt.', 'ROLE_BEERLOVER');
-INSERT INTO users (username,password_hash,role) VALUES ('connor', '$2a$10$Xvtmp.i43yBEE8uzmX8fhObRWOmAZiHrz7Px3OM1wFVNjup6IYGXq', 'ROLE_BEERLOVER');
+INSERT INTO users (username,password_hash,role) VALUES ('ricardo', '$2a$10$afuUAXrV/kKQjkr7v1OLg.AsebZxt2lg4rzhoUMTxSsP/Jqfw4MUS', 'ROLE_BREWER');
+INSERT INTO users (username,password_hash,role) VALUES ('jane', '$2a$10$aFWHuZWqFAyl8b.ou2fzFuMvXfRJ/HqI1uM85Ah0N6TxxVBmOG4O.', 'ROLE_BREWER');
+INSERT INTO users (username,password_hash,role) VALUES ('spencer', '$2a$10$U0q1o1SW5nCa6BHlfNqLHug8J0mcOjv6zBcnV66OYbJamaID.HIt.', 'ROLE_BREWER');
+INSERT INTO users (username,password_hash,role) VALUES ('connor', '$2a$10$Xvtmp.i43yBEE8uzmX8fhObRWOmAZiHrz7Px3OM1wFVNjup6IYGXq', 'ROLE_BREWER');
 INSERT INTO users (username,password_hash,role) VALUES ('david', '$2a$10$.ufkAXypZWEphMv8R36jzO8uDnGa2ZznqhIftkicReHrRG.9OZ8Ei', 'ROLE_BEERLOVER');
 INSERT INTO users (username,password_hash,role) VALUES ('isaac', '$2a$10$ql9/Rl0Z.kQNDgHjwbxa0enTEQSx70QbuO9EHfACifLUsBNkKupp6', 'ROLE_BEERLOVER');
 INSERT INTO users (username,password_hash,role) VALUES ('justin', '$2a$10$0RYBCU2qis96/DdJJQyU8ek4T65tdJcxMVSphDdWQ3.U2ljK3LWTG', 'ROLE_BEERLOVER');
@@ -50,11 +50,178 @@ VALUES('IPA', 'This West Coast IPA is loaded with a fresh tropical fruit charact
 INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
 VALUES('Hazy IPA', 'A hazy IPA packed with a jaw-dropping quantity of Mosaic hops. This aromatic IPA has a ripe juicy character with notes of fresh peaches and mango. A soft, silky mouthfeel is complemented by a smooth, refreshing finish. HOPS: Mosaic, Simcoe, Eureka', 'https://cdn.shopify.com/s/files/1/2653/8302/products/madewest-hasy-ipa-4pack_1800x1800.jpg?v=1636506584', 7, 'IPA - New England / Hazy', 'true');
 
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES(	
+		'Ventura Light', 
+		'Golden, full-flavored light lager made with the finest ingredients to achieve a clean crisp refreshing finish. Pairs perfect with the beach, the trails and everything in between. HOPS: Cascade', 
+		'https://cdn.shopify.com/s/files/1/2653/8302/products/Ventura_Light_White_Background_1800x1800_2305d084-3745-4588-9460-2ce39f8a6078_720x.jpg?v=1575926498', 
+		4.2, 'Lager - American Light', 'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Short-Lived Series W/ Fieldwork', 
+		'West Coast IPA hopped with New Zealand''s finest Nelson, Nectaron and Waimea hops. This thiol rich Southern Hemisphere hop combination unleashes tropical fruit aromas with notes of intense grapefruit, passion fruit and guava while the lean malt profile leaves this IPA finishing dry and refreshing. *Thiol Boosted*', 
+		'https://cdn.shopify.com/s/files/1/2653/8302/products/PhotoRoom_20221031_105337_1800x1800.jpg?v=1667336337', 
+		7, 
+		'IPA - American', 
+		'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Chief Peak', 
+		'You’ve gone from shrubland to woodland to highland. When you stop to sip on a Chief Peak, the piney hops are ﬁrst to arrive. The tropical tones of passion fruit, orange, and gooseberry aren’t far behind. Look out on the valley and trace the contours of the mountain. Breathe in the thinning air, wipe the sweat from your brow, and take another well-earned sip. Taste Buds: Tropical, Dank, Citrus ', 
+		'https://cdn.shopify.com/s/files/1/0464/5673/3857/products/5e71d03e-907c-45ab-8954-0578fd5ad003_76006a6b-5194-495e-b292-920fe8fe1335_720x.jpg?v=1604644826', 
+		7, 
+		'IPA', 
+		'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Spectro', 
+		'You’ve been hiking for hours through a dense fog. Your body is tired and sticky, but you push on toward your goal. Finally, you reach a clearing, the mist lifts, and there sits your reward: an icy can of Spectro Hazy IPA. You sit and sip as you relish the mystery of the haze that swirls around you and in your can. Taste Buds: Pineapple, Peach, Guava', 
+		'https://cdn.shopify.com/s/files/1/0464/5673/3857/products/5e71d03f-07f8-4b5a-a470-0578fd5ad003_eb852637-dcf2-4703-892f-dfbc37699462_720x.jpg?v=1604644835', 
+		6.3, 
+		'Hazy IPA', 
+		'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Dos Topas', 
+		'The summer sun dips low. You brush the salt from your skin after a long day of ocean swims and sandy beach towel naps. Now, your reward: you sink into your hammock with a Dos Topas in hand and let the cool, crisp taste wash over you. So tomorrow, when those warm sunshiny rays crest the mountain peaks, you’ll be ready for round dos. Taste Buds: Cracker, Maize, Crisp', 
+	   	'https://cdn11.bigcommerce.com/s-k0y7xl9dpv/images/stencil/508x508/products/4310/38433/445560__87530.1645727308.jpg?c=1', 
+	   	4.5, 
+	   	'Lager',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Level Line', 
+		'As you wade in the current, the work week fades to gray. When the line strikes and you begin that delicate dance, there''s no choice but to be in the moment. The refreshing citrus aromas and woodsy notes are like the creek passing through the forest. It''s a West Coast Pale Ale for a West Coast way of life. Taste Buds: Lemongrass, Tangerine, Woodsy', 
+	   	'https://s.yimg.com/ny/api/res/1.2/rjfuPmTsd_L4gALnlOtE2Q--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTU0NQ--/https://media.zenfs.com/en/men_s_journal_478/e15844734b82073b89216effd4afd4ee', 
+	   	5, 
+	   	'Pale Ale',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Pixie', 
+		'White Wheat Ale with Pixie Tangerine Zest & Marmalade, Indian Coriander, and Chamomile, rolled oats, and raw white wheat. Bittering provided by Bravo Hops. Medium Light Body, Round Mouthfeel, Mild balanced bitterness that gives way to slight sweetness. Tasting notes include: apple candy, citrus blossom, orange citrus, vanilla.', 
+	   	'https://cdn.shopify.com/s/files/1/0319/3965/7863/products/Can-4pack-Pixie-LowRes_1024x1024@2x.jpg?v=1649294645', 
+	   	6.5, 
+	   	'White Wheat Ale',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES 		('Sugar Bush', 
+		'Hop forward Pale Ale with Wild Sumac. Anybody who wants an IPA will enjoy this beer. It is basically an IPA but slightly less bitter, and with the addition of lemony sumac berries. We buy the sumac from a foraging supplier. Sugar Bush is the name of the Sumac that grows locally here in Ojai. We have a baby Sugar Bush Sumac growing by the telephone pole next to the patio fence.', 
+	   	'https://cdn.shopify.com/s/files/1/0319/3965/7863/products/SugarBush-4pack-LowRes_1024x1024@2x.jpg?v=1649299615', 
+	   	6.8, 
+	   	'Pale Ale',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Cali Bock', 
+		'Hop forward Hybrid Lager. Combination of California Common Lager and Helles Bock Lager. Clean medium bodied, with a higher alcohol content than most lagers. Tasting notes include: Caramel, Graham Cracker, toast, sweet herbs.', 
+	   	'https://cdn.shopify.com/s/files/1/0319/3965/7863/products/Can-4pack-Calibock-LowRes_1024x1024@2x.jpg?v=1649293194', 
+	   	6.3, 
+	   	'Hybrid Lager',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Ranchero', 
+		'Mexicali Lager. Traditional Amber Mexican Lager using California Common lager yeast, and Pacific Crest Noble Hops. Clean, dry, finish with light malty sweetness and toasted grain finish. Tasting notes include: wheat bread crust, and minty herbs.', 
+	   	'https://cdn.shopify.com/s/files/1/0319/3965/7863/products/IMG_3356_1024x1024@2x.jpg?v=1649304994	https://cdn.shopify.com/s/files/1/0319/3965/7863/products/IMG_3356_1024x1024@2x.jpg?v=1649304994', 
+	   	5, 
+	   	'Mexican Lager',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'New Moon', 
+		'Wild Oat Stout, brewed with Local raw oat and wheat straw. Smooth medium bodied black Ale, with a medium dry finish and mild carbonation. Tasting notes include: Chocolate, Coffee, graham cracker, hay.', 
+	   	'https://assets.untappd.com/photos/2022_11_06/c1eefae64cd9eb3580d99b77cd2ffb22_1280x1280.jpg	https://assets.untappd.com/photos/2022_11_06/c1eefae64cd9eb3580d99b77cd2ffb22_1280x1280.jpg', 
+	   	6, 
+	   	'Stout',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Arbolada', 
+		'Black Lager or “schwarzbier” brewed for a very light body so that it can be drunk in hot weather, and infused with whole Mexican Vanilla beans for a slight creaminess. This beer is named after the shady oak stand in the northwest of the Ojai Valley. Mexican Vanilla is currently worth more than silver by the ounce. Tasting notes include: vanilla, chocolate, coffee, Cream Soda.', 
+	   	'https://cdn.shopify.com/s/files/1/0319/3965/7863/products/Arbolada-Can-4pack-LowRes_1024x1024@2x.jpg?v=1649301433', 
+	   	5.8, 
+	   	'Black Lager',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Airhog Apricot Ale', 
+		'Straight up an experiment, Airhog Apricot Ale is hazy like crazy! With each sip of Airhog, you’ll pick up on the many aromas of sweet apricot blossoms and tart fruit as you draw the glass near. Deep, rich apricot flesh, then tart apricot notes dance on the palate and lead to a sweet apricot finish with a lingering fruit tartness.', 
+	   	'https://craftpeak-cooler-images.imgix.net/poseidon-brewing-co/crowler-frontback.png?auto=compress%2Cformat&fit=scale&h=909&ixlib=php-3.3.0&w=600&wpsize=tile&s=cf032fb3676e1d62f47399853b4404e4', 
+	   	7, 
+	   	'Fruit IPA',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Amphibious Amber Ale', 
+		'Amphibious Amber Ale is an intensely malt centric beer accenting the aromatic and full-bodied flavors of English malts. Toasted caramel and mild chocolate aromatics are present with a hint of character from our house yeast. A great beer for the malt lover in all of us.', 
+	   	'https://craftpeak-cooler-images.imgix.net/poseidon-brewing-co/Amphibious-frontback.png?auto=compress%2Cformat&fit=scale&h=909&ixlib=php-3.3.0&w=600&wpsize=tile&s=ecd9546fbf39e1b810043b64062b0f77', 
+	   	5, 
+	   	'Amber Ale',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Battleship Blonde', 
+		'A great way to kick off your visit at Poseidon Brewing Co., our first-ever experimental blonde ale uses copious amounts of Caravienne malt with a dash of Columbus hops. We kept the hop addition to a minimum to help bring out the wonderful malt and fruit characteristics of this brew. Clean aromas of honey, nectarines, peaches and apricots are present. Smooth and refreshing!', 
+	   	'https://craftpeak-cooler-images.imgix.net/poseidon-brewing-co/Battleship-frontback.png?auto=compress%2Cformat&fit=scale&h=909&ixlib=php-3.3.0&w=600&wpsize=tile&s=61a9fc393365eab42963f1065a93ff12', 
+	   	6, 
+	   	'Blonde Ale',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Blown Off Course IPA', 
+		'So we set out to brew an India Pale Ale, and it didn’t quite turn out like we’d planned, hence the name, Blown Off Course IPA. But being blown off course isn’t a bad thing when there is white grapefruit involved and the hops used include Cascade, Simcoe and Horizon.', 
+	   	'https://craftpeak-cooler-images.imgix.net/poseidon-brewing-co/BlownOffCourse-frontback.png?auto=compress%2Cformat&fit=scale&h=909&ixlib=php-3.3.0&w=600&wpsize=tile&s=69841ca9dac9cea7182dc9867067b20c', 
+	   	7, 
+	   	'Fruit IPA',
+	   	'true');
+
+INSERT INTO beers (beer_name, beer_description, image, abv, beer_type, is_active)
+VALUES (
+		'Borchard Beer Cali Common', 
+		'Borchard Beer is a California Common, also known as a Steam Beer, that is a 100% American-style lager brewed with a lager yeast strain that ferments at warmer ale-like temperatures. This method of fermenting dates back to the late 1800s in California when refrigeration was a great luxury.', 
+	   	'https://craftpeak-cooler-images.imgix.net/poseidon-brewing-co/Borchard-frontback.png?auto=compress%2Cformat&ixlib=php-3.3.0&s=d30db24ffb542cd130a07b71b49f819c', 
+	   	5.8, 
+	   	'California Common',
+	   	'true');
+
 -- BREWERIES_BEERS
 INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(1, 1);
 INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(1, 2);
 INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(1, 3);
 INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(1, 4);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(1, 5);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(1, 6);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(2, 7);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(2, 8);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(2, 9);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(2, 10);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(3, 11);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(3, 12);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(3, 13);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(3, 14);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(3, 15);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(3, 16);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(4, 17);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(4, 18);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(4, 19);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(4, 20);
+INSERT INTO breweries_beers (brewery_id, beer_id) VALUES(4, 21);
 
 -- REVIEWS
 INSERT INTO reviews (review_text, user_id, image) 
