751a80e8c87b2a5dc71867c1284d84237a9ea4b1
Ricardo Cruz <rickblender@hotmail.com>
2022-12-09 13:15:19 -0600

    added business hours to brewery page

--- capstone/vue/src/components/BreweryInfo.vue
+++ capstone/vue/src/components/BreweryInfo.vue
@@ -1,24 +1,117 @@
 <template>
-    <div>
-        <h1>{{brewery.breweryName}}</h1>
-        <img v-bind:src="brewery.breweryLogo" v-bind:alt="brewery.breweryName + ' logo'">
-        <img v-bind:src="brewery.breweryImage" v-bind:alt="brewery.breweryName + ' image'">
-        <p>{{ address }}</p>
-        <p>{{ brewery.phoneNumber }}</p>
-        <p>{{ brewery.emailAddress }}</p>
-        <p>{{ brewery.websiteUrl }}</p>
-        <p>{{ brewery.breweryHistory }}</p>
-    </div>
+  <div>
+    <h1>{{breweryName}}</h1>
+    <img v-bind:src="breweryLogo" v-bind:alt="breweryName + ' logo'">
+    <img v-bind:src="breweryImage" v-bind:alt="breweryName + ' image'">
+    <p>{{ address }}</p>
+    <p>{{ phoneNumber }}</p>
+    <p>{{ emailAddress }}</p>
+    <p>{{ websiteUrl }}</p>
+    <!-- <div v-for="hour in hours" v-bind:key="hour.hoursId">
+      <label>{{ hour.day }}</label>
+      <input type="time" v-model="hour.openingHour">
+      <input type="time" v-model="hour.closingHour">
+    </div> -->
+    <table>
+      <thead>
+        <tr>
+          <th>Day</th>
+          <th>Open</th>
+          <th>Close</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="hour in businessHours" v-bind:key="hour.hoursId">
+          <td>{{ hour.day }}</td>
+          <td>{{ hour.openingHour }}</td>
+          <td>{{ hour.closingHour }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p>{{ breweryHistory }}</p>
+  </div>
 </template>
 
 <script>
+import breweryService from '../services/BreweryService.js';
+
 export default {
     name: 'brewery-info',
-    props: ['brewery'],
+    data() {
+      return {
+        breweryName: '',
+        websiteUrl: '',
+        emailAddress: '',
+        phoneNumber: '',
+        breweryLogo: '',
+        breweryImage: '',
+        breweryHistory: '',
+        active: false,
+        streetAddress: '',
+        city: '',
+        state: '',
+        zipcode: '',
+        hours: [],
+      }
+  },
     computed: {
         address() {
-            return `${this.brewery.address.streetAddress}, ${this.brewery.address.city}, ${this.brewery.address.state} ${this.brewery.address.zipcode}`;
+            return `${this.streetAddress}, ${this.city}, ${this.state} ${this.zipcode}`;
+        },
+        businessHours() {
+          return this.hours.map(hour => {
+            return {
+              hoursId: hour.hoursId,
+              day: hour.day,
+              openingHour: this.displayTime(hour.openingHour),
+              closingHour: this.displayTime(hour.closingHour)
+            };
+          })
         }
+    },
+    methods: {
+    getCurrentBrewery() {
+      breweryService.get(this.$route.params.id)
+      .then(response => {
+        if (response.status === 200) {
+          this.$store.commit('SET_CURRENT_BREWERY', response.data);
+          this.breweryName = response.data.breweryName;
+          this.websiteUrl = response.data.websiteUrl;
+          this.emailAddress = response.data.emailAddress;
+          this.phoneNumber = response.data.phoneNumber;
+          this.breweryLogo = response.data.breweryLogo;
+          this.breweryImage = response.data.breweryImage;
+          this.breweryHistory = response.data.breweryHistory;
+          this.active = response.data.active;
+          this.streetAddress = response.data.address.streetAddress;
+          this.city = response.data.address.city;
+          this.state = response.data.address.state;
+          this.zipcode = response.data.address.zipcode;
+          this.hours = response.data.hours
+        } else {
+          console.log(response.status);
+        }
+      })
+      .catch(error => {
+          let errorMessage;
+          if (error.response) {
+            errorMessage = `${error.response.status}: ${error.response.data.error}, ${error.response.data.message}`;
+          } else if (error.request) {
+            errorMessage = 'Error submitting form. Server could not be reached.';
+          } else {
+            errorMessage = 'Error submitting form. Request could not be created.';
+          }
+          console.log(errorMessage);
+        });
+    },
+    displayTime(timeString) {
+      const now = new Date();
+      let tempDate = new Date(now.toDateString() + ' ' + timeString);
+      return tempDate.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
     }
+  },
+  created() {
+    this.getCurrentBrewery();
+  }
 }
 </script>
--- capstone/vue/src/views/Brewery.vue
+++ capstone/vue/src/views/Brewery.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <navigation></navigation>
-    <brewery-info v-bind:brewery="brewery"></brewery-info>
+    <brewery-info></brewery-info>
     <router-link v-bind:to="{ name: 'breweries'}">Back to Breweries</router-link>
   </div>
 </template>
@@ -9,44 +9,13 @@
 <script>
 import Navigation from '../components/Navigation.vue';
 import BreweryInfo from '../components/BreweryInfo.vue';
-import breweryService from '../services/BreweryService.js';
 
 export default {
   name: 'brewery',
   components: {
     Navigation,
     BreweryInfo
-  },
-  computed: {
-    brewery() {
-      return this.$store.state.currentBrewery;
-    }
-  },
-  methods: {
-    getCurrentBrewery() {
-      breweryService.get(this.$route.params.id)
-      .then(response => {
-        if (response.status === 200) {
-          this.$store.commit('SET_CURRENT_BREWERY', response.data);
-        } else {
-          console.log(response.status);
-        }
-      })
-      .catch(error => {
-          let errorMessage;
-          if (error.response) {
-            errorMessage = `${error.response.status}: ${error.response.data.error}, ${error.response.data.message}`;
-          } else if (error.request) {
-            errorMessage = 'Error submitting form. Server could not be reached.';
-          } else {
-            errorMessage = 'Error submitting form. Request could not be created.';
-          }
-          console.log(errorMessage);
-        });
-    }
-  },
-  created() {
-    this.getCurrentBrewery();
   }
+  
 }
 </script>
