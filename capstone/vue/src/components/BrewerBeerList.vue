b720e04974796988f63e8510ae68c42e97fffded
Ricardo Cruz <rickblender@hotmail.com>
2022-12-10 11:43:36 -0600

    toggle active/inactive functionality

--- capstone/vue/src/components/BrewerBeerList.vue
+++ capstone/vue/src/components/BrewerBeerList.vue
@@ -4,8 +4,8 @@
     <div v-for="beer in this.$store.state.brewerBeers" v-bind:key="beer.beerId">
       <h3>{{ beer.beerName }}</h3>
       <button v-on:click="viewBeer(beer.beerId)">View</button>
-      <button v-show="beer.active">Active</button>
-      <button v-show="!beer.active">Inactive</button>
+      <button v-show="beer.active" v-on:click="toggleActive(beer)">Active</button>
+      <button v-show="!beer.active" v-on:click="toggleActive(beer)">Inactive</button>
     </div>
   </div>
 </template>
@@ -30,12 +30,34 @@ export default {
           if (error.response) {
             errorMessage = `${error.response.status}: ${error.response.data.error}, ${error.response.data.message}`;
           } else if (error.request) {
-            errorMessage = 'Error submitting form. Server could not be reached.';
+            errorMessage = 'Server could not be reached.';
           } else {
-            errorMessage = 'Error submitting form. Request could not be created.';
+            errorMessage = 'Request could not be created.';
           }
           console.log(errorMessage);
         })
+    },
+    toggleActive(beer) {
+      beer.active = !beer.active;
+      beerService.delete(beer.beerId)
+      .then(response => {
+        if (response.status === 200) {
+          console.log(response.status);
+        } else {
+          console.log(response.status);
+        }
+      })
+      .catch(error => {
+          let errorMessage;
+          if (error.response) {
+            errorMessage = `${error.response.status}: ${error.response.data.error}, ${error.response.data.message}`;
+          } else if (error.request) {
+            errorMessage = 'Server could not be reached.';
+          } else {
+            errorMessage = 'Request could not be created.';
+          }
+          console.log(errorMessage);
+        });
     },
     viewBeer(beerId) {
       this.$router.push({ name: 'beers', params: { id: beerId }});
