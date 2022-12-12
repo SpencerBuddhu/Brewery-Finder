81320dc3012c34b512221e4c49a75d0a810e0b48
Ricardo Cruz <rickblender@hotmail.com>
2022-12-12 10:16:35 -0600

    changed layout of login page

--- capstone/vue/src/views/Login.vue
+++ capstone/vue/src/views/Login.vue
@@ -4,9 +4,46 @@
       <div class="main-card">
         <h1>Brewery Finder</h1>
         <p class="description">Join our community of beer lovers. Find breweries and view their fine selection of beers. Leave a rating or review of a beer and follow your favorite brewery to stay up-to-date.</p>
+        <div class="form-container-alt">
+          <form class="form-signin-alt" @submit.prevent="login">
+          <h2 class="welcome">Welcome!</h2>
+          <div
+            class="alert alert-danger"
+            role="alert"
+            v-if="invalidCredentials"
+          >Invalid username and password!</div>
+          <div
+            class="alert alert-success"
+            role="alert"
+            v-if="this.$route.query.registration"
+          >Thank you for registering, please sign in.</div>
+          <!-- <label for="username" class="sr-only">Username</label> -->
+          <input
+            type="text"
+            id="username"
+            class="form-control"
+            placeholder="Username"
+            v-model="user.username"
+            required
+            autofocus
+          />
+          <!-- <label for="password" class="sr-only">Password</label> -->
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+            v-model="user.password"
+            required
+          />
+          
+          <button type="submit">Sign in</button>
+          <router-link :to="{ name: 'register' }">Need an account?</router-link>
+        </form>
+      </div>
       </div>
     </div>
-    <div id="login" class="text-center">
+    <!-- <div id="login" class="text-center">
       <form class="form-signin" @submit.prevent="login">
         <h2 class="welcome">Welcome!</h2>
         <div
@@ -18,9 +55,9 @@
           class="alert alert-success"
           role="alert"
           v-if="this.$route.query.registration"
-        >Thank you for registering, please sign in.</div>
+        >Thank you for registering, please sign in.</div> -->
         <!-- <label for="username" class="sr-only">Username</label> -->
-        <input
+        <!-- <input
           type="text"
           id="username"
           class="form-control"
@@ -28,9 +65,9 @@
           v-model="user.username"
           required
           autofocus
-        />
+        /> -->
         <!-- <label for="password" class="sr-only">Password</label> -->
-        <input
+        <!-- <input
           type="password"
           id="password"
           class="form-control"
@@ -42,7 +79,7 @@
         <button type="submit">Sign in</button>
         <router-link :to="{ name: 'register' }">Need an account?</router-link>
       </form>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -90,20 +127,24 @@ export default {
   height: 100vh;
   justify-content: center;
   align-items: center;
-  padding: 96px 160px;
+  /* padding: 96px 160px; */
   background-color: hsl(13, 100%, 72%);
 }
 .cta {
   font-family: Ubuntu, sans-serif;
   /* text-align: center; */
   color: hsl(0, 0%, 100%);
-  padding: 96px;
+  /* padding: 96px; */
+  /* margin: 0 auto; */
 }
 .main-card {
-  padding: 64px;
+  width: 1040px;
   background-color: hsl(207, 13%, 34%);
   /* border: 2px solid black; */
   border-radius: 16px;
+  text-align: center;
+  padding: 64px;
+  box-shadow: 8px 8px 8px rgb(64, 64, 64);
 }
 .main-card > h1 {
   font-size: 64px;
@@ -123,12 +164,36 @@ export default {
   text-align: center;
   border-radius: 16px;
 }
+.form-container-alt {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 32px;
+}
+.form-signin-alt {
+  display: flex;
+  width: 500px;
+  /* height: 400px; */
+  flex-direction: column;
+  align-items: center;
+  background-color: hsl(0, 0%, 100%);
+  font-family: Ubuntu, sans-serif;
+  /* text-align: center; */
+  border-radius: 16px;
+  padding: 32px 0;
+  box-shadow: 8px 8px 8px rgb(48, 48, 48);
+}
 .form-signin > h2 {
   font-size: 32px;
   color: hsl(208, 49%, 24%);
   margin: 32px 0;
 }
-.form-signin > button {
+.form-signin-alt > h2 {
+  font-size: 32px;
+  color: hsl(208, 49%, 24%);
+  /* margin: 32px 0; */
+}
+button {
   width: 175px;
   height: 50px;
   color: hsl(0, 0%, 100%);
@@ -138,7 +203,7 @@ export default {
   font-size: 16px;
   margin: 16px 0;
 }
-.form-signin > button:hover {
+button:hover {
   background-color: hsl(237, 17%, 21%);
   font-weight: bold;
   cursor: pointer;
